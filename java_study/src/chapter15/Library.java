72eab73fc75ad1418a84fb847cfa84e5637af419
tkdrl9900 <tkdrl9900@gmail.com>
2024-09-13 16:18:49 +0900

    20240913

---
+++ java_study/src/chapter15/Library.java
@@ -0,0 +1,42 @@
+package chapter15;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Library {
+	private Map<String, Book> books; // 도서관의 모든 책을 관리하는 Map
+	
+	// 생성자
+	public Library() {
+		this.books = new HashMap<String, Book>();
+	} // 책은 없지만 도서관 구조물이 남는다
+	
+	
+	
+	// 책 추가 메서드
+	public void addBook(Book book) {
+		books.put(book.getIsbn(), book);
+		System.out.println(book.getTitle() + "이(가) 도서관에 추가되었습니다.");
+	}
+	
+	// 제목으로 책 검색
+	// 람다 표현식 & 스트림 API
+	public List<Book> searchBooksByTitle(String title) {
+		return books.values().stream()
+				.filter(book -> book.getTitle().equalsIgnoreCase(title))
+				.collect(Collectors.toList());
+	}
+	
+	// 저자로 책 검색
+	public List<Book> searchBookByAuthor(String author) {
+		return books.values().stream()
+				.filter(book -> book.getAuthor().equalsIgnoreCase(author))
+				.collect(Collectors.toList());
+	}
+	
+	// 도서관 데이터를 저장하는 메서드
+	
+	// 도서관 데이터를 로드하는 메서드
+}
